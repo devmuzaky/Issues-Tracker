168a39f4930b45aaeb43a8dea6dbbe5459d63b6f
Moe Zaky <devmuzaky@gmail.com>
2023-04-16 17:52:01 +0200

    Auto-generated commit [ci skip]

--- src/app/models/issue.ts
+++
@@ -1,8 +0,0 @@
-export interface Issue {
-  issueNo: number;
-  title: string;
-  description: string;
-  priority: 'low' | 'high';
-  type: 'Feature' | 'Bug' | 'Documentation';
-  completed?: Date;
-}
